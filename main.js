72aa1cdf8160f5858a6ce170700dccb023242fb9
Raslav Milutinovic <raslav.milutinovic@gmail.com>
2017-10-18 13:52:54 +0200

    Adding hilbert curve d2xy calculation.

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+Point.prototype = {
+  rotate: function (n, x, y) {
+      if (y === 0) {
+          if (x === 1) {
+              this.x = n-1 - this.x;
+              this.y = n-1 - this.y;
+          }
+
+          var t  = this.x;
+          this.x = this.y;
+          this.y = t;
+      }
+  }
+};
+
+function d2xy(n, d) {
+    var p = new Point(0, 0);
+    var rx = 0;
+    var ry = 0;
+    var t=d;
+
+    for (var s=1; s<n; s*=2) {
+        rx = 1 & (t/2);
+        ry = 1 & (t ^ rx);
+        p.rotate(s, rx, ry);
+        p.x += s * rx;
+        p.y += s * ry;
+        t /= 4;
+    }
+    return p;
+}
+
 function loadData(pools, url) {
     $.getJSON( url, function( data ) {
         if (data["next"] === null) {
